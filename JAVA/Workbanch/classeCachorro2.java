f27255b112c85f11a1cad6db30d409c3c1897a2d
Victor <yguinhoqm123@gmail.com>
2023-06-27 11:19:46 -0300

    Conceitos C

--- JAVA/Workbanch/classeCachorro2.java
+++ JAVA/Workbanch/classeCachorro2.java
@@ -6,12 +6,18 @@ public class classeCachorro2 {
         classeCachorro cachorro1 = new classeCachorro();
 
         cachorro1.nome = "Puppy";
-        System.out.println();
         cachorro1.cor = "Marron";
         cachorro1.altura = 25;              // objetos
         cachorro1.peso = 5.5;
         cachorro1.tamanhoDoRabo = 5;
+    
 
-        System.out.println(cachorro1);
+        cachorro1.latir();
+        cachorro1.pegar();
+        System.out.println("O cachorro pegou uma " + cachorro1.pegar());
+
+        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
+        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
+        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
 }
 }
